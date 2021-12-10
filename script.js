4e5465aa9b8056a58fe65e77e33e95c9c463878b
Noah Kim <heanokim@gmail.com>
2021-12-10 13:08:18 -0800

    finished creating the initial version of etch-a-sketch. It does not include the optional feature where the colors are randomized and the colored-in grid turns black slowly after each additional grid is colored in

--- script.js
+++ script.js
@@ -1,29 +1,81 @@
-const container = document.getElementById('container');
+// Ask the user how many grids should each side have?
 
-// let n = prompt('How many rows and columns do you want?');
+// let n = prompt();
+let n = 16
 
-let n = 16;
+// create container that will hold the grid
 
-// function that creates rows
+// function that creates ROWS
 function createRows() {
+    const container = document.getElementById('container');
     for (i = 0; i < n; i++) {
         let row = document.createElement('div');
         row.classList.add('row');
-        container.append(row);
+        container.appendChild(row);
     }
 };
 
 createRows();
 
-// add columns to each row
-const rows = document.querySelectorAll('.row');
-rows.forEach((eachRow) => {
-    for (i = 0; i < n; i++) {
-        let column = document.createElement('div');
-        column.classList.add('column');
-        eachRow.append(column);
+// add COLUMNS/GRIDS to each row
+
+function createColumns() {
+    const rows = document.querySelectorAll('.row');
+    rows.forEach((eachRow) => {
+        for (i = 0; i < n; i++) {
+            let column = document.createElement('div');
+            column.classList.add('column');
+            eachRow.appendChild(column);
+        };
+    });
+}
+
+createColumns();
+
+// adds effect where the grid gets colored in if the mouse HOVERS over it
+
+function colorGrid() {
+    const columns = document.querySelectorAll('.column');
+    columns.forEach((grid) => {
+        grid.addEventListener('mouseenter', e => {
+            grid.style.backgroundColor = '#000000';
+        });
+    });
+}
+
+colorGrid();
+
+// removes all children from container
+function removeGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
-})
+}
+
+// add a RESET button
+const resetButton = document.querySelector('.reset');
+
+resetButton.addEventListener('click', e => {
+    n = prompt();
+    if (n <= 100) {
+        removeGrid();
+        createRows();
+        createColumns();
+        colorGrid();
+    } else {
+        alert("Please pick a number that less than or equal to 100." + "\n" +
+            "\n" + "We don't want to crash the computer!");
+    }
+});
+
+console.log(container);
+
+// just checking to see what the container elements look like
+
+
+// function that creates columns - OLD CAN DELETE BUT WILL KEEP FOR HISTORY
+// doesn't work because I can't call the row variable from this function... maybe
+// if I used const or var it could work but I used 'let'
 
 // function createColumns() {
 //     let rows = document.querySelectorAll('row');
@@ -35,5 +87,3 @@ rows.forEach((eachRow) => {
 // };
 
 // createColumns();
-
-console.log(container)
